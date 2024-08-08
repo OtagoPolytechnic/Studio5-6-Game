7e96bf5ffe5bd2d0e26d0389e75f154b6c085967
Erika Stuart (1000106562) <erikat.stuart@gmail.com>
2024-08-08 16:15:28 +1200

    Added overlap checking

--- Assets/Scripts/Erika/RoomGeneration.cs
+++ Assets/Scripts/Erika/RoomGeneration.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-* Randomly choose room from array
-* place either 50 units above, below, or left or right from the last room
-* cannot overlap if a room is already there
+* Picks a random room
+* If no rooms are placed, place the room at 0,0
+* Add room to list
 * 
 */
 
 public class RoomGeneration : MonoBehaviour
 {
-    [SerializeField] private GameObject[] rooms;
+    [SerializeField] private GameObject[] roomPrefabs; // A list of the rooms that will be pulled from the prefabs
+    private List<GameObject> currentRooms = new List<GameObject>(); // A list of the generated rooms so none overlap
+    //private List<Vector3> placedRoomPos = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,44 +29,62 @@ public class RoomGeneration : MonoBehaviour
 
     private GameObject ChooseRoom()
     {
-        return rooms[Random.Range(0, rooms.Length)];
+        Debug.Log("Choosing Room");
+        return roomPrefabs[Random.Range(0, roomPrefabs.Length)];
     }
 
     private void PlaceRoom(GameObject room)
     {
-        Vector3 position = Vector3.zero;
-        if (transform.childCount == 0)
+        if (currentRooms.Count == 0)
         {
-            position = Vector3.zero;
+            Debug.Log("Placing Room at 0,0");
+            room.transform.position = Vector3.zero;
+            currentRooms.Add(room);
         }
         else
         {
-            Transform lastRoom = transform.GetChild(transform.childCount - 1);
-            position = lastRoom.position;
-            int direction = Random.Range(0, 4);
-            switch (direction)
+            Debug.Log("Placing Room at Random Position");
+            switch(Random.Range(1, 5)) // 1 left, 2, up, 3 right, 4 down
             {
-                case 0:
-                    position += new Vector3(0, 25, 0);
-                    break;
                 case 1:
-                    position += new Vector3(0, -25, 0);
+                    room.transform.position = new Vector3(currentRooms[currentRooms.Count - 1].transform.position.x - 10, currentRooms[currentRooms.Count - 1].transform.position.y, 0); // the x position of the last room placed - 10
+                    CheckPosition(room);
                     break;
                 case 2:
-                    position += new Vector3(25, 0, 0);
+                    room.transform.position = new Vector3(currentRooms[currentRooms.Count - 1].transform.position.x, currentRooms[currentRooms.Count - 1].transform.position.y + 10, 0); // the y position of the last room placed + 10
+                    CheckPosition(room);
                     break;
                 case 3:
-                    position += new Vector3(-25, 0, 0);
+                    room.transform.position = new Vector3(currentRooms[currentRooms.Count - 1].transform.position.x + 10, currentRooms[currentRooms.Count - 1].transform.position.y, 0); // the x position of the last room placed + 10
+                    CheckPosition(room);
+                    break;
+                case 4:
+                    room.transform.position = new Vector3(currentRooms[currentRooms.Count - 1].transform.position.x, currentRooms[currentRooms.Count - 1].transform.position.y - 10, 0); // the y position of the last room placed - 10
+                    CheckPosition(room);
                     break;
             }
         }
-        Instantiate(room, position, Quaternion.identity, transform);
+
+    }
+
+    private void CheckPosition(GameObject room) //gets called in place room
+    {
+        for (int i = 0; i < currentRooms.Count; i++) // check through the list of currently placed rooms
+        {
+            // if none of the current rooms overlap with the next room, place the room
+            if (room.transform.position != currentRooms[i].transform.position)
+            {
+                room.transform.position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+                currentRooms.Add(room);
+            }
+        }
     }
 
     private void GenerateRoom()
     {
         for (int i = 0; i < 10; i++)
         {
+            Debug.Log("Generating Room " + i);
             GameObject room = ChooseRoom();
             PlaceRoom(room);
         }
