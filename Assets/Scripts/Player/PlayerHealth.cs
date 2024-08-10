03c587950959efa4a0762d8c735c99828bb230d6
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-08-11 10:36:47 +1200

    fix: removed strange using namespaces from scripts

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
-using PlasticPipe.PlasticProtocol.Messages;
 
 public class PlayerHealth : MonoBehaviour
 {
