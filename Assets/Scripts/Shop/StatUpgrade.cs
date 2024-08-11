a9bfcd2cc606e894bd4d6d6f7c6573344f5054ae
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-08-11 15:46:17 +1200

    add: working on upgrade stats system

--- Assets/Scripts/Shop/StatUpgrade.cs
+++ Assets/Scripts/Shop/StatUpgrade.cs
@@ -8,6 +8,9 @@ public class StatUpgrade : MonoBehaviour
     public int level = 1;
     public float baseCost = 2f;
     public Stats stat;
+    public float value;
+    public bool percentUpgrade;
+    public float upgradeModifier;
     public TextMeshProUGUI currentLevelText;
 
     public TextMeshProUGUI nextLevelText;
@@ -15,19 +18,40 @@ public class StatUpgrade : MonoBehaviour
     public TextMeshProUGUI titleText;
 
 
+    private float GetStatValue(Stats stat)
+    {
+        switch(stat)
+        {
+            case Stats.SPEED:
+                return TopDownMovement.moveSpeed;
+                break;
+            case Stats.DAMAGE:
+                return PlayerHealth.damage;
+                break;
+            case Stats.HEALTH:
+                return PlayerHealth.maxHealth;
+                break;
+        }
+        return -1;
 
+    }
     private void Start()
     {
-        currentLevelText.text = level.ToString();
-        nextLevelText.text = "";
+        if (GetStatValue(stat) != -1)
+        {
+            value = GetStatValue(stat);
+        }
+        else
+        {
+            value = 0;
+        }
+        currentLevelText.text = value.ToString();
+        nextLevelText.text = (PlayerHealth.instance.UpgradeStat(value,upgradeModifier,percentUpgrade)).ToString();
         costText.text = "$" + baseCost.ToString();
         titleText.text = ConvertStatToString(stat);
     }
-    public void Initialise()
-    {
 
-    }
-    public string ConvertStatToString(Stats stat)
+    private string ConvertStatToString(Stats stat)
     {
         switch(stat)
         {
