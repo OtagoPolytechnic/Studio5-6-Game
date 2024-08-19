4f289860559cf47ff94d9c17afee83e86b2f16df
YJunX <xushen006@gmai.com>
2024-08-19 13:29:13 +1200

    fixed the overlay and remove the code don't need

--- Assets/Scripts/Lantern/Lantern.cs
+++ Assets/Scripts/Lantern/Lantern.cs
@@ -19,19 +19,13 @@ public class Lantern : MonoBehaviour
         }
     }
 
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //   {
-    //        ToggleLantern();
-    //    }
-    //}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             ToggleLantern();
+            Destroy(gameObject);
         }
     }
 }
