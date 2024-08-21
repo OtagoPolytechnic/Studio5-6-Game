8fdaefd46c3f73ba03f55263e0cca0ae26acece7
3MKJOE58 <ALTAYY1@student.op.ac.nz>
2024-08-22 02:33:53 +1200

    updated scripts to allow spawning more than 2 enemies

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -4,21 +4,64 @@ using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour
 {
-    public GameObject meeleEnemy1;
-    public GameObject rangedEnemy2;
+    public GameObject meleeEnemy;
+    public GameObject rangedEnemy;
+    public GameObject heavyEnemy;
+    public GameObject fastEnemy;
+    public float minDistance = 1.0f; // Minimum distance between enemies
+
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
 
     void Start()
     {
-        int numberOfSprites = Random.Range(1, 6);
+        int numberOfEnemies = Random.Range(2, 8);
 
-        for (int i = 0; i < numberOfSprites; i++)
+        for (int i = 0; i < numberOfEnemies; i++)
         {
-            GameObject chosenPrefab = Random.Range(0, 2) == 0 ? meeleEnemy1 : rangedEnemy2;
+            GameObject chosenPrefab;
+            int enemyType = Random.Range(0, 3);
 
-            // Instantiate a new sprite
-            GameObject newSprite = Instantiate(chosenPrefab);
+            if (enemyType == 0)
+            {
+                chosenPrefab = meleeEnemy;
+            }
+            else if (enemyType == 1)
+            {
+                chosenPrefab = rangedEnemy;
+            }
+            else if (enemyType == 2)
+            {
+                chosenPrefab = heavyEnemy;
+            }
+            else
+            {
+                chosenPrefab= fastEnemy;
+            }
 
-            newSprite.transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            Vector2 spawnPosition;
+            bool positionValid;
+
+            do
+            {
+                // Generate a random position
+                spawnPosition = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+                positionValid = true;
+
+                // Check the distance to all previously spawned enemies
+                foreach (GameObject enemy in spawnedEnemies)
+                {
+                    if (Vector2.Distance(spawnPosition, enemy.transform.position) < minDistance)
+                    {
+                        positionValid = false;
+                        break;
+                    }
+                }
+            }
+            while (!positionValid); // Keep trying until a valid position is found
+
+            // Instantiate the enemy and add it to the list of spawned enemies
+            GameObject newEnemy = Instantiate(chosenPrefab, spawnPosition, Quaternion.identity);
+            spawnedEnemies.Add(newEnemy);
         }
     }
 }
