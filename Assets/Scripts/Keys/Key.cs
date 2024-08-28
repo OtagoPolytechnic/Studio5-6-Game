59d3d775046a4558b883a26f33d8fc87ac5e3698
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-08-29 11:09:59 +1200

    add: a single key can now unlock an object

--- Assets/Scripts/Keys/Key.cs
+++ Assets/Scripts/Keys/Key.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// This class manages a single key item, the key is used to unlock an UnlockableObject such as a door.
+/// </summary>
+/// <remarks>
+/// Author: Chase Bennett-Hill
+/// Date: 29 / 08 / 2024
+/// </remarks>
 public class Key : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// The UnlockableObject that this key unlocks.
+    /// </summary>
+    [HideInInspector] public UnlockableObject targetObject; 
+
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            if (targetObject == null)
+            {
+                Debug.LogError("Key does not have a target object.");
+                return;
+            }
+            targetObject.UseKey(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/Keys/UnlockableObject.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This class manages an object that can be unlocked by a key.
+/// </summary>
+/// <remarks>
+/// Author: Chase Bennett-Hill
+/// Date: 29 / 08 / 2024
+/// </remarks>
+public class UnlockableObject : MonoBehaviour
+{
+    /// <summary>
+    /// The set of keys that are required to unlock this object.
+    /// </summary>
+    public List<Key> requiredKeys;
+    public Sprite unlockedSprite;
+    public Sprite lockedSprite;
+    private SpriteRenderer spriteRenderer;
+    /// <summary>
+    /// Whether or not this object is currently locked.
+    /// </summary>
+    public bool isLocked = true;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (isLocked)
+        {
+            spriteRenderer.sprite = lockedSprite;
+        }
+
+        foreach (Key key in requiredKeys)
+        {
+            key.targetObject = this;
+        }
+    }
+
+    public void UseKey(Key key)
+    {
+        if (requiredKeys.Contains(key))
+        {
+            requiredKeys.Remove(key);
+            Destroy(key.gameObject);
+            if (requiredKeys.Count == 0)
+            {
+                Unlock();
+
+            }
+            else
+                Debug.Log("Key Obtained for " + gameObject.name + " " + requiredKeys.Count + " keys remaining.");
+        }
+        else
+        {
+            Debug.Log("This key does not unlock this object.");
+        }
+    }
+
+
+    public void Unlock()
+    {
+        isLocked = false;
+        spriteRenderer.sprite = unlockedSprite;
+        Debug.Log("You have unlocked the " + gameObject.name );
+
+    }
+}
