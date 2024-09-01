477398a335cc58f9c5f944121815962b343c360d
unknown <xushen006@gmail.com>
2024-09-02 07:56:32 +1200

    add boss enmey spawner

---
+++ Assets/Scripts/Jun/BossSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour
+{
+    public GameObject bossPrefab;  
+    public Transform spawnLocation; 
+    private bool hasSpawned = false; 
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasSpawned) 
+        {
+            SpawnBoss();
+            hasSpawned = true;
+        }
+    }
+
+    
+    private void SpawnBoss()
+    {
+        if (bossPrefab != null && spawnLocation != null)
+        {
+            Instantiate(bossPrefab, spawnLocation.position, spawnLocation.rotation);
+            Debug.Log("Boss has been spawned!");
+        }
+        else
+        {
+            Debug.LogError("BossPrefab or SpawnLocation is not set!");
+        }
+    }
+}
