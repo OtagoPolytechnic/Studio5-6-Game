a6d78f0e0f9e7ee4fff6a04f7f85f484be9bef49
3MKJOE58 <ALTAYY1@student.op.ac.nz>
2024-09-04 15:33:35 +1200

    UPDATE: chooses between four enemies to spawn

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour
 {
-    public GameObject meeleEnemy1;
-    public GameObject rangedEnemy2;
+    public GameObject meeleEnemy;
+    public GameObject rangedEnemy;
+    public GameObject fastEnemy;
+    public GameObject heavyEnemy;
 
     void Start()
     {
-        int numberOfSprites = Random.Range(1, 6);
+        int numberOfEnemies = Random.Range(2, 7); // Random number between 2 and 6
 
-        for (int i = 0; i < numberOfSprites; i++)
+        for (int i = 0; i < numberOfEnemies; i++)
         {
-            GameObject chosenPrefab = Random.Range(0, 2) == 0 ? meeleEnemy1 : rangedEnemy2;
+            // Randomly choose one of the four enemy types
+            GameObject chosenPrefab;
+            int randomChoice = Random.Range(0, 4);
 
-            // Instantiate a new sprite
-            GameObject newSprite = Instantiate(chosenPrefab);
+            switch (randomChoice)
+            {
+                case 0:
+                    chosenPrefab = meeleEnemy;
+                    break;
+                case 1:
+                    chosenPrefab = rangedEnemy;
+                    break;
+                case 2:
+                    chosenPrefab = fastEnemy;
+                    break;
+                case 3:
+                    chosenPrefab = heavyEnemy;
+                    break;
+                default:
+                    chosenPrefab = meeleEnemy;
+                    break;
+            }
 
-            newSprite.transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            // Instantiate the chosen enemy
+            GameObject newEnemy = Instantiate(chosenPrefab);
+
+            // Set a random position for the enemy
+            newEnemy.transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+
         }
     }
 }
