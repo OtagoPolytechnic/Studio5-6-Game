045c3ad40ae164c666a1b46fa0b58949dc74335c
Lorna Hart (1000114994) <HARTLR3@student.op.ac.nz>
2024-09-05 13:06:04 +1200

    chore: change back to game manager method

--- Assets/Scripts/Princess.cs
+++ Assets/Scripts/Princess.cs
@@ -5,23 +5,11 @@ using UnityEngine;
 
 public class Princess : MonoBehaviour
 {
-    public GameObject victoryScreen;
-    public TMP_Text victoryText;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            //GameManager.Instance.Victory();
-            Victory();
+            GameManager.Instance.Victory();
         }
-    }
-
-    //temp until bugs in Game Manager are fixed
-    private void Victory()
-    {
-        victoryText.color = Color.green;
-        victoryText.text = "Victory!";
-        victoryScreen.SetActive(true);
     }
 }
