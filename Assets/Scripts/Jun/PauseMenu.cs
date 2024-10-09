07f17b8e4b5b662677b3ae503a3823837f982449
unknown <xushen006@gmail.com>
2024-10-09 18:51:06 +1300

    ADD: pause mune code in

---
+++ Assets/Scripts/Jun/PauseMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        GameIsPaused = true;
+    }
+
+    public void MainMenu()
+    {
+        GameIsPaused = false;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
