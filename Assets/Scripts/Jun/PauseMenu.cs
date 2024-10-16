97a7eb68f54dce51f0f87b09833ae8d69f6e01d0
unknown <xushen006@gmail.com>
2024-10-16 20:50:42 +1300

    Finshed the pause menu and ready for review

--- Assets/Scripts/Jun/PauseMenu.cs
+++ Assets/Scripts/Jun/PauseMenu.cs
@@ -9,6 +9,26 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
 
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
